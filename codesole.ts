3755d756e5404a4a3640d023decfbe83ea231c4a
kreuzerk <kevin.kreuzer90@icloud.com>
2019-09-30 08:02:06 +0200

    refactor(languagecompiler): remove language compiler

--- codesole.ts
+++ codesole.ts
@@ -1,17 +1,12 @@
-import {LanguageCompiler} from './language-compiler';
 import {xml} from './languages/xml';
 import {HighlightEngine} from './highlight-engine';
 
 export class Codesole {
 
-    private languageCompiler: LanguageCompiler;
     private highlightEngine: HighlightEngine;
 
     constructor() {
-        this.languageCompiler = new LanguageCompiler();
-        const languageDefinition = xml();
-        this.languageCompiler.compileLanguage(languageDefinition);
-        this.highlightEngine = new HighlightEngine(languageDefinition);
+        this.highlightEngine = new HighlightEngine(xml());
     }
 
     public highlight(value) {
